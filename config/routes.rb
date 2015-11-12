9f06dcdaf23cacb20514a66024f792e4b42b77b8
Frederique Petris <frederique.petris@gmail.com>
2015-11-12 18:52:10 +0100

    modification route doses.destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 root 'cocktails#index'
 
   resources :cocktails do
-    resources :doses, only: [:new, :create]
+    resources :doses, only: [:new, :create, :destroy]
   end
 
-resources :doses, only: [:destroy]
+# resources :doses, only: [:destroy]
 # delete "doses/:id", to: "doses#destroy"
 
 end
