1628b57206f5add90dbcd4fba888d716b06ef8d4
Frederique Petris <frederique.petris@gmail.com>
2015-11-13 15:51:41 +0100

    ajout DELETE cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,12 @@
 class CocktailsController < ApplicationController
 
+  before_action :set_cocktail, only: [:show, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @doses = Dose.all
     @dose = Dose.new
   end
@@ -17,13 +18,23 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to @cocktail
+      #idem redirect_to cocktail_path(@cocktail)
     else
      render :new
-    end
+   end
+  end
+
+  def destroy
+   @cocktail.destroy
+   redirect_to root_path
   end
 
   private
+
+  def set_cocktail
+   @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :picture)
